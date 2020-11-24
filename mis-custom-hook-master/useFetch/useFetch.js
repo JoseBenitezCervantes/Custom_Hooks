19fb0ddba3dafe90d92b828a87d011f3a550eeff
JoseBenitezCervantes <51679969+JoseBenitezCervantes@users.noreply.github.com>
2020-11-24 10:37:08 -0600

    Update useFetch.js

--- mis-custom-hook-master/useFetch/useFetch.js
+++ mis-custom-hook-master/useFetch/useFetch.js
@@ -1,44 +1,38 @@
 import { useState, useEffect, useRef } from 'react';
 
+export const useFetch = (url, method = 'GET') => {
+  const isMounted = useRef(true);
+  const [state, setState] = useState({ data: null, loading: true, error: null });
 
-export const useFetch = ( url ) => {
-    
-    const isMounted = useRef(true);
-    const [state, setState] = useState({ data: null, loading: true, error: null });
+  useEffect(() => {
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
 
-    useEffect( () => {
-        return () => {
-            isMounted.current = false;
-        }
-    }, [])
+  const getData = async () => {
+    try {
+      const res = await fetch(url, { method });
+      const data = await res.json();
+      setState({
+        loading: false,
+        error: null,
+        data,
+      });
+    } catch (error) {
+      console.log('error', error);
+      setState({
+        data: {},
+        loading: false,
+        error: true,
+      });
+    }
+  };
 
+  useEffect(() => {
+    setState({ data: null, loading: true, error: null });
+    getData();
+  }, [url]);
 
-    useEffect( () => {
-
-        setState({ data: null, loading: true, error: null });
-
-        fetch( url )
-            .then( resp => resp.json() )
-            .then( data => {
-
-                if ( isMounted.current ) {
-                    setState({
-                        loading: false,
-                        error: null,
-                        data
-                    });
-                }
-
-            })
-            .catch( () => {
-                setState({
-                    data: null,
-                    loading: false,
-                    error: 'No se pudo cargar la info'
-                })
-            })
-
-    },[url])
-
-    return state;
-}
+  return state;
+};
