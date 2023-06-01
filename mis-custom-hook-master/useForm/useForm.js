50135d0b0c8395c5a199f0fed9085fd4dd0dac7a
JoseBenitezCervantes <51679969+JoseBenitezCervantes@users.noreply.github.com>
2023-05-31 21:12:22 -0600

    Update useForm.js

--- mis-custom-hook-master/useForm/useForm.js
+++ mis-custom-hook-master/useForm/useForm.js
@@ -1,24 +1,39 @@
-import { useState } from "react"
+import { haveEmptyKeys } from "@/utils";
+import { useEffect, useState } from "react";
 
-
-export const useForm = ( initialState = {} ) => {
-    
-    const [values, setValues] = useState(initialState);
-
-    const reset = () => {
-        setValues( initialState );
+export const useForm = (initialState = {}) => {
+  const [values, setValues] = useState(initialState);
+  const [haveEmpty, setHaveEmpty] = useState(true);
+  useEffect(() => {
+    if (haveEmptyKeys(values)) {
+      setHaveEmpty(true);
+    } else {
+      setHaveEmpty(false);
     }
+  }, [values]);
 
+  const reset = () => {
+    setValues(initialState);
+  };
 
-    const handleInputChange = ({ target }) => {
+  const handleInputChange = ({ target }: any) => {
+    setValues({
+      ...values,
+      [target.name]: target.value,
+    });
+  };
 
-        setValues({
-            ...values,
-            [ target.name ]: target.value
-        });
+  return [values, handleInputChange, reset, haveEmpty];
+};
 
+export const haveEmptyKeys = (obj: any) => {
+  for (let clave in obj) {
+    const actualValue = obj[clave];
+    const isEmpty =
+    actualValue === undefined || actualValue === null || actualValue === "";
+    if (obj.hasOwnProperty(clave) && isEmpty) {
+      return true; // Retorna true si encuentra una clave vacía
     }
-
-    return [ values, handleInputChange, reset ];
-
-}
+  }
+  return false; // Retorna false si no encuentra claves vacías
+};
